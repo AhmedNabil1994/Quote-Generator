d7731f11d5a954ede24d47448f9523861349ba83
AhmedNabil1994 <94ahmed.nabil@gmail.com>
2024-11-24 22:13:05 +0200

    edit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,62 @@
-var quotes = [
+var authors = [
   {
-    author: "Oscar Wilde",
+    name: "Oscar Wilde",
     quote: "Be yourself; everyone else is already taken.",
     image: "image_1",
   },
   {
-    author: "Frank Zappa",
+    name: "Frank Zappa",
     quote: "So many books, so little time.",
     image: "image_2",
   },
   {
-    author: "Mahatma Gandhi",
+    name: "Mahatma Gandhi",
     quote: "Be the change that you wish to see in the world.",
     image: "image_3",
   },
   {
-    author: "Marcus Tullius Cicero",
+    name: "Marcus Tullius Cicero",
     quote: "A room without books is like a body without a soul.",
     image: "image_4",
   },
   {
-    author: "Friedrich Nietzsche",
+    name: "Friedrich Nietzsche",
     quote: "Without music, life would be a mistake.",
     image: "image_5",
   },
   {
-    author: "Nikolas Schreck",
+    name: "Nikolas Schreck",
     quote: "SILENCE you miserable cretins!.",
     image: "image_6",
   },
+  {
+    name: "Khaled Hosseini",
+    quote: "For you a thousand times over.",
+    image: "image_7",
+  },
+  {
+    name: "Tad Williams",
+    quote: "General was unsurprised, and raised.",
+    image: "image_8",
+  },
+  {
+    name: "Mae West",
+    quote: "You only live once, but if you do it right, once is enough.",
+    image: "image_9",
+  },
+  {
+    name: "Mark Twain",
+    quote: "If you tell the truth, you don't have to remember anything.",
+    image: "image_10",
+  },
 ];
-
 var usedIndex = -1;
 /**
-  * function to generate quote text
-  * ,Author name, and Author Image 
-*/
+ * function to generate quote text
+ * ,Author name, and Author Image
+ */
 function getQuote() {
+  changeBtnText()
   var generatedIndex;
   /*  this loop ensure that no repetation
       keep generating random indices
@@ -44,17 +64,19 @@ function getQuote() {
       if they are different , exit the loop and display the quotes
   */
   do {
-    generatedIndex = Math.floor(Math.random() * quotes.length);
+    generatedIndex = Math.floor(Math.random() * authors.length);
   } while (generatedIndex === usedIndex);
   usedIndex = generatedIndex;
   document.getElementById("quote-text").innerText =
-    quotes[generatedIndex].quote;
+    authors[generatedIndex].quote;
   document.getElementById("quote-author").innerText =
-    quotes[generatedIndex].author;
+    authors[generatedIndex].name;
   document.getElementById(
     "author-image"
-  ).src = `./images/${quotes[generatedIndex].image}.jpg`;
-  document.getElementById("author-image").alt = quotes[generatedIndex].image;
+  ).src = `./images/${authors[generatedIndex].image}.jpg`;
+  document.getElementById("author-image").alt = authors[generatedIndex].image;
 }
 
-
+function changeBtnText () {
+  document.getElementById("quote-btn").innerText = "New Quote";
+}
